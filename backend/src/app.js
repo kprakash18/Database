8451f93055d2ffc92dde3026b41cf909c28dbcc1
kprakash18 <iamkprakash18@gmail.com>
2026-05-02 18:15:59 +0530

    moved the router path from server to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,16 @@
 import express  from 'express'
 import cors from "cors";
-import dotenv from "dotenv";
-import { pool } from './config/db.js'
+
+
+// custom routes
+import getAllSamples from './routers/sampleRoutes.js';
 
 const app = express() ;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
+app.use('/api', getAllSamples) ;
 
 
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config() ;
 const port = process.env.PORT ;
 
-// custom routes
-import getAllSamples from './routers/sampleRoutes.js';
 
-app.use('/api', getAllSamples) ;
   
 
 app.listen(port, ()=>{
