cbfb08d36cdcc0163d08a161c88299e5476ca473
kprakash18 <iamkprakash18@gmail.com>
2026-05-04 00:24:46 +0530

    create api to fetch lineage of bacteria

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,7 @@ import getAllSamples from './routers/sampleRoutes.js';
 import getAllSamplesById from './routers/getSampleByIdRoute.js' ;
 import getBacterialCompositionBySampleId from './routers/getBacterialCompositionRouter.js';
 import getTaxonUsingNcbi from './routers/fetchNcbiTaxonIdRouter.js'
+import fetchLineageNcbi from './routers/fetchLineageRouter.js' ;
 const app = express() ;
 
 // middleware
@@ -16,6 +17,7 @@ app.use('/api', getAllSamples) ;
 app.use('/api', getAllSamplesById) ;
 app.use('/api', getBacterialCompositionBySampleId) ;
 app.use('/api', getTaxonUsingNcbi) ;
+app.use('/api', fetchLineageNcbi) ;
 
 
 
