fe536db8dbe110f3cc905a2e2d3a438943eed979
kprakash18 <iamkprakash18@gmail.com>
2026-05-04 00:42:03 +0530

    add api to parse xml and extract lineage and convert to json

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ import getAllSamplesById from './routers/getSampleByIdRoute.js' ;
 import getBacterialCompositionBySampleId from './routers/getBacterialCompositionRouter.js';
 import getTaxonUsingNcbi from './routers/fetchNcbiTaxonIdRouter.js'
 import fetchLineageNcbi from './routers/fetchLineageRouter.js' ;
+import parseLineageXML from './routers/parseLineageXMLRouter.js' ;
 const app = express() ;
 
 // middleware
@@ -18,6 +19,7 @@ app.use('/api', getAllSamplesById) ;
 app.use('/api', getBacterialCompositionBySampleId) ;
 app.use('/api', getTaxonUsingNcbi) ;
 app.use('/api', fetchLineageNcbi) ;
+app.use('/api', parseLineageXML) ;
 
 
 
