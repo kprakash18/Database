34bc1883205f6bbcdc899cb0ee88f8b4ab51ea54
kprakash18 <iamkprakash18@gmail.com>
2026-05-05 23:48:54 +0530

    updated the service file of getClassLevelComposition -> get composition by rank and sample_id

--- backend/src/services/getClassLevelComposition.js
+++ backend/src/services/getClassLevelComposition.js
@@ -1,32 +1,39 @@
 import { pool } from "../config/db.js";
 const allowedRanks = [
-  'domain',
-  'phylum',
-  'class',
-  'order',
-  'family',
-  'genus',
-  'species',
-  'strain',
-  'unknown'
-];
-export const getClassLevelCompositionBySampleId = async (sampleId,rank) =>{
-    if(!allowedRanks.includes(rank)) {
-      throw new Error("Invalid Taxonomy Rank") ;
+    "domain",
+    "phylum",
+    "class",
+    "order",
+    "family",
+    "genus",
+    "species",
+    "strain",
+    "unknown",
+  ];
+export const getClassLevelCompositionBySampleId = async (sampleId,rank) => {
+    if (!allowedRanks.includes(rank)) {
+        throw new Error("Invalid taxonomy rank");
     }
-    const result = await pool.query(`
-      SELECT
-        bc.comp_id,
-        bc.sample_id,
-        bc.tax_id,
-        bc.taxon_name,
-        bc.reported_rank,
-        bc.relative_abundance
+  const result  = await pool.query(`
+    SELECT
+      bc.comp_id,
+      bc.sample_id,
+      s.food_name,
+      s.description,
+      bc.taxon_name,
+      bc.tax_id,
+      bc.relative_abundance,
+      bc.presence,
+      bc.is_dominant,
+      bc.reported_rank,
+      bc.measurement_type
       FROM bacterial_composition bc
+      JOIN samples s 
+      ON bc.sample_id = s.sample_id
       WHERE bc.sample_id = $1
-        AND bc.reported_rank = $2
+      AND bc.reported_rank = $2
       ORDER BY bc.relative_abundance DESC NULLS LAST;
-      `,
-      [sampleId, rank] );
-      return result.rows ;
-    } ;
+  `,
+  [sampleId, rank]);
+  return result.rows;
+};
